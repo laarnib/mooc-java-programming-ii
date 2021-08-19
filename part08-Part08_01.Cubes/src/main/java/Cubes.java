cd6c64ecd99abd224d618359b194ef689d6fc10e
Laarni Bernardo <laarnijb@gmail.com>
2021-08-19 09:57:45 -0400

    add comments and close scanner

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -6,6 +6,7 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        // Ask for input until user enters "end"
         while(true) {
             String input = scanner.nextLine();
             
@@ -13,12 +14,18 @@ public class Cubes {
                 break;
             } 
             
+            // Check if input is a number
             try {
+                /* Parse input to an integer and print to the screen 
+                the cube of the integer */
                 int number = Integer.valueOf(input);
                 System.out.println(number * number * number);
             } catch (NumberFormatException e) {
                 System.out.println("Input is not a number. Please enter a number.");
             }
         }
+        
+        // Close the scanner
+        scanner.close();
     }
 }
